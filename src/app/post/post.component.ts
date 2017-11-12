d6f8183e9f8bca25f97ebd553f0607a2036b2b94
pierdomenico reitano <pierdomenico@lotrek.it>
2017-11-13 00:15:37 +0100

    created page for signle article

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { AngularFireDatabase } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import { ActivatedRoute} from '@angular/router';
@@ -20,10 +21,13 @@ interface PostId extends Post {
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
-
+  title: string;
+  content: string;
   postDoc: AngularFirestoreDocument<Post>;
   singlePost: Observable<Post>;
-
+  idref: any;
+  postTitle: string;
+  postContent: string;
   constructor(
     private afs: AngularFirestore,
     private route: ActivatedRoute,
@@ -32,6 +36,18 @@ export class PostComponent implements OnInit {
   ngOnInit() {
       this.postDoc = this.afs.doc('posts/' + this.route.snapshot.params['id']);
       this.singlePost = this.postDoc.valueChanges();
+  }
+
+  updatePost(id) {
+    console.log(this.title);
+    console.log(this.content);
+      this.afs.collection('posts').doc(id).update({
+        'title': this.title,
+        'content': this.content
+      }
+    );
+
+
   }
 
 }
